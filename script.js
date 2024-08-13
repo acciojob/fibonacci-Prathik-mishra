aea85681c6e22c657ee96602d5284131e8ff4055
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2024-08-13 18:06:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
 // your code here
+	let a=0, b=1, c,i;
+	for (let i = 0; i <= num;i++){
+		 c=a+b;
+		a=b;
+		b=c;
+	}
+	return b;
+
+	 
 }
+let num = prompt();
+console.log("the  fibonacci numbers are"+ fibonacci(num));
 
 module.exports = fibonacci;
