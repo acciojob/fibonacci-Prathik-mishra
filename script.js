64cb2d879e391f1a6586bc5f7f74ad857c66aed4
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2024-08-21 20:39:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-let num=9;
+let num=1;
 function fibonacci(num) {
 // your code here
 	let a=0, b=1, result;
-	if(num == 0){
-		return a;
-	}
+	if(num === 0 || num === 1){
+		console.log(a);
+       }
+       else{
+   
 	for (let i = 2; i <= num;i++){
 		 result=a+b;
 		a=b;
 		b=result;
 	}
-	return b;
+	console.log(b);
+}
 }
 fibonacci(num);
+
 
 
 
